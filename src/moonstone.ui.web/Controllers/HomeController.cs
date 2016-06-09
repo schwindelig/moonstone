d37edaab9b6db672c84431e5ea630fd8c74c707d
schwindelig <david.szoeke@gmail.com>
2016-06-09 20:59:17 +0200

    added basic login and logout functionality.

--- src/moonstone.ui.web/Controllers/HomeController.cs
+++ src/moonstone.ui.web/Controllers/HomeController.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using moonstone.ui.web.Models;
 using System.Web.Mvc;
 
 namespace moonstone.ui.web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        public HomeController(Current current) : base(current)
+        {
+        }
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public string Seeed()
+        {
+            var res = this.Current.AuthenticationHub.UserManager.CreateAsync(
+                new core.models.User
+                {
+                    Email = "david.szoeke@gmail.com"
+                }, "mooT$12!d9");
+
+            if (!res.Result.Succeeded)
+            {
+                return "failed to create user";
+            }
+
+            return "seeded";
         }
     }
 }
